5c32239e0b7107851efd25e9e8250e6ee8e21f83
Mohamed Daniel Al Mouiee <d.almouiee@gmail.com>
2017-09-27 10:21:57 +1000

    Update chief.pl

--- chief.pl
+++ chief.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 # Date: 27/09/2017
-# A perl program to track the leader of a community given input files
+# A perl script to track the leader of a community given input files
 
 open F, '<', $ARGV[0];
 while(<F>) {
