57437386a30fdc83daddce56dc6a196d957b4e55
Mohamed Daniel Al Mouiee <d.almouiee@gmail.com>
2017-10-02 23:03:30 +1100

    Update chief.pl

--- chief.pl
+++ chief.pl
@@ -2,13 +2,11 @@
 # Date: 27/09/2017
 # A perl script to track the leader of a community given input files
 
-open F, '<', $ARGV[0];
-while(<F>) {
+while(<>) {
 	($first, $second) = $_ =~ /^(\w+) (\w+)/;
 	$map{$first}++;
 	$map{$second}++;
 }
-close F;
 $max = $second;
 foreach $key(sort keys %map) {
 	$max = $key if $map{$max} < $map{$key};
